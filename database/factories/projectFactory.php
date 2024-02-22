bb3ebdcfbe4f60e8186c398b2838749302b2355a
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-22 15:55:32 +0100

    add: aggiunto seeder e factory

---
+++ database/factories/projectFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\project>
+ */
+class projectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nome'=>fake()->word(),
+            'inizio_progetto'=>fake()->date(),
+            'descrizione'=>fake()->paragraph(),
+        ];
+    }
+}
---
+++ database/factories/typeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\type>
+ */
+class typeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'in_gruppo'=>fake()->boolean(),
+            'risorse_usate'=>fake()->word(),
+        ];
+    }
+}
